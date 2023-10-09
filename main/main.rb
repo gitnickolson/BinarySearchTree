8658ca09d268fe76c42c1285ae4165bce057aaa7
gitnickolson <nicknickolson@web.de>
2023-10-09 11:37:42 +0200

    Some additions - Added find method - Added height method - Added depth method

--- main/main.rb
+++ main/main.rb
@@ -26,29 +26,21 @@ class Tree
   def insert(value)
     current_node = root
     new_node = Node.new(value)
-    direction = nil
-    nil_node = :nothing
 
-    while nil_node != nil
-      pp current_node
-      pp nil_node
-      return "The given value is already in the tree" if current_node.value == value
+    while current_node != nil
+      return p "The given value is already in the tree (#{value})" if current_node.value == value
       if current_node.value < new_node.value
         direction = :right
         last_node = current_node
-        nil_node = current_node.right_node
         current_node = current_node.right_node
 
       else
         direction = :left
         last_node = current_node
-        nil_node = current_node.left_node
         current_node = current_node.left_node
       end
     end
 
-    p direction
-    p current_node.inspect
     if direction == :left
       last_node.left_node = new_node
     else
@@ -57,7 +49,95 @@ class Tree
   end
 
   def delete(value)
+    current_node = root
+    new_node = Node.new(value)
+    last_node = nil
 
+    while current_node != nil
+      break if last_node == value
+      if current_node.value < new_node.value
+        direction = :right
+        last_node = current_node
+        current_node = current_node.right_node
+
+      else
+        direction = :left
+        last_node = current_node
+        current_node = current_node.left_node
+      end
+    end
+
+    last_node
+  end
+
+  def find(value)
+    current_node = root
+    new_node = Node.new(value)
+
+    while current_node != nil
+      return p "#{current_node}" if current_node.value == value
+      if current_node.value < new_node.value
+        direction = :right
+        last_node = current_node
+        current_node = current_node.right_node
+
+      else
+        direction = :left
+        last_node = current_node
+        current_node = current_node.left_node
+      end
+    end
+  end
+
+  def height(value)
+    current_node = root
+    new_node = Node.new(value)
+    height_count = 0
+    post_value = false
+
+    while current_node != nil
+      if current_node.value == value || post_value == true
+        if post_value == true
+          height_count += 1
+        end
+        post_value = true
+      end
+
+      if current_node.value < new_node.value
+        direction = :right
+        last_node = current_node
+        current_node = current_node.right_node
+
+      else
+        direction = :left
+        last_node = current_node
+        current_node = current_node.left_node
+      end
+    end
+
+    p height_count
+  end
+
+  def depth(value)
+    current_node = root
+    new_node = Node.new(value)
+    depth_count = 0
+
+    while current_node != nil
+      return p depth_count if current_node.value == value
+      if current_node.value < new_node.value
+        direction = :right
+        last_node = current_node
+        current_node = current_node.right_node
+        depth_count += 1
+
+      else
+        direction = :left
+        last_node = current_node
+        current_node = current_node.left_node
+        depth_count += 1
+      end
+    end
   end
 
   def pretty_print(node = @root, prefix = '', is_left = true)
@@ -69,18 +149,24 @@ end
 
 
 class Node
-  attr_accessor :value, :left_node, :right_node, :comparison_result
+  attr_accessor :value, :left_node, :right_node
   def initialize(value, left_node = nil, right_node = nil)
     @value = value
     @left_node = left_node
     @right_node = right_node
-    comparison_result = left_node <=> right_node
+
   end
 end
 
 BST = Tree.new([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324])
-p BST.insert(5)
+BST.insert(5)
 BST.insert(29)
+BST.delete(29)
+BST.find(67)
+BST.depth(67)
+BST.height(67)
+BST.depth(3)
+BST.height(3)
 
 BST.pretty_print
 
