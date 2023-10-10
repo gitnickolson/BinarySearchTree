27926dcda08c575c362ffa5eb89594885ac38e85
gitnickolson <nicknickolson@web.de>
2023-10-10 11:20:14 +0200

    Bugfix - Delete method now doesn't crash the program when trying to delete the root

--- main/main.rb
+++ main/main.rb
@@ -17,10 +17,26 @@ class Tree
     else
       split_array = array.each_slice((array.length / 2.0).round).to_a
     end
-    left_node = build_tree(split_array[0])
-    right_node = build_tree(split_array[1])
+    left = build_tree(split_array[0])
+    right = build_tree(split_array[1])
 
-    Node.new(node_value, left_node, right_node)
+    Node.new(node_value, left, right)
+  end
+
+  def find(value)
+    current_node = root
+
+    while current_node != nil
+      return current_node if current_node.value == value
+      if current_node.value < value
+        previous_node = current_node
+        current_node = current_node.right
+
+      else
+        previous_node = current_node
+        current_node = current_node.left
+      end
+    end
   end
 
   def insert(value)
@@ -31,125 +47,111 @@ class Tree
       return p "The given value is already in the tree (#{value})" if current_node.value == value
       if current_node.value < new_node.value
         direction = :right
-        last_node = current_node
-        current_node = current_node.right_node
+        previous_node = current_node
+        current_node = current_node.right
 
       else
         direction = :left
-        last_node = current_node
-        current_node = current_node.left_node
+        previous_node = current_node
+        current_node = current_node.left
       end
     end
 
     if direction == :left
-      last_node.left_node = new_node
+      previous_node.left = new_node
     else
-      last_node.right_node = new_node
+      previous_node.right = new_node
     end
   end
 
   def delete(value)
     current_node = root
-    last_node = nil
+    previous_node = nil
+    previous_node_save = nil
 
-    while current_node.value != value
+    while current_node != nil && current_node.value != value
       if current_node.value < value
         direction = :right
-        last_node = current_node
-        current_node = current_node.right_node
+        previous_node = current_node
+        current_node = current_node.right
 
       else
         direction = :left
-        last_node = current_node
-        current_node = current_node.left_node
+        previous_node = current_node
+        current_node = current_node.left
       end
     end
+    return pp "The value doesn't exist in the tree!" if current_node == nil
 
-    return p "The value doesn't exist in the tree!" if current_node == nil
-    return_value = current_node
-    previous_last_node = last_node
-
-    if current_node.left_node == nil && current_node.right_node == nil #Keine child nodes
-      if direction == :right
-        previous_last_node.right_node = nil
-      else
-        previous_last_node.left_node = nil
-      end
-
-    elsif current_node.left_node == nil #Eine child node
-      if direction == :right
-        previous_last_node.right_node = current_node.right_node
-      else
-        previous_last_node.left_node = current_node.right_node
-      end
-
-    elsif current_node.right_node == nil #Eine child node
-      if direction == :right
-        previous_last_node.right_node = current_node.left_node
-      else
-        previous_last_node.left_node = current_node.left_node
-      end
-
-    else #Zwei child nodes
-      right_save = current_node.right_node
-      left_save = current_node.left_node
-
-      current_node = current_node.right_node
-      while current_node.left_node != nil
-        last_node = current_node
-        current_node = current_node.left_node
-      end
-
-      last_node.left_node = current_node.right_node
-
-      current_node.left_node = left_save
-      current_node.right_node = right_save
-
-      if direction == :right
-        previous_last_node.right_node = current_node
-      else
-        previous_last_node.left_node = current_node
-      end
+    deleted_value = current_node
+    if current_node == root
+      previous_node_save = current_node
+    else
+      previous_node
+      previous_node_save = previous_node
     end
 
-    return_value
-  end
+    right_save = current_node.right
+    left_save = current_node.left
 
-  def find(value)
-    current_node = root
+    if current_node.left == nil && current_node.right == nil # Keine child node
+      replace_node(previous_node_save, current_node, direction, 0)
 
-    while current_node != nil
-      return p "#{current_node}" if current_node.value == value
-      if current_node.value < value
-        last_node = current_node
-        current_node = current_node.right_node
+    elsif current_node.left == nil || current_node.right == nil #Eine child node
+      replace_node(previous_node_save, current_node, direction, 1)
 
+    elsif current_node.left != nil && current_node.right != nil 
+      current_node = current_node.right
+      while current_node.left != nil
+        previous_node = current_node
+        current_node = current_node.left
+      end
+
+      current_node.left = left_save
+      current_node.right = right_save
+
+      previous_node.left = nil
+
+      if deleted_value == root
+        replace_node(previous_node_save, current_node, direction, 2, root)
       else
-        last_node = current_node
-        current_node = current_node.left_node
+        replace_node(previous_node_save, current_node, direction, 2)
       end
     end
+    deleted_value
   end
 
-  def height(value)
+  def replace_node(previous_node_save, node, direction, children, root = nil)
+    if direction == :right
+      case children
+      when 0
+        previous_node_save.right = nil
 
-  end
+      when 1
+        previous_node_save.right = node.left
 
-  def depth(value)
-    current_node = root
-    depth_count = 0
+      when 2
+        if root == nil
+          previous_node_save.value = node.value
+        else
+          previous_node_save.right = node
+        end
+      end
 
-    while current_node != nil
-      return p depth_count if current_node.value == value
-      if current_node.value < value
-        last_node = current_node
-        current_node = current_node.right_node
-        depth_count += 1
+    else
+      case children
+      when 0
+        previous_node_save.left = nil
 
-      else
-        last_node = current_node
-        current_node = current_node.left_node
-        depth_count += 1
+      when 1
+        previous_node_save.left = node.left
+
+      when 2
+        if root == nil
+          previous_node_save.left = node
+        else
+          previous_node_save.value = node.value
+        end
       end
     end
   end
@@ -162,8 +164,8 @@ class Tree
     queue << current_node
 
     while current_node != nil
-      queue << current_node.left_node
-      queue << current_node.right_node
+      queue << current_node.left
+      queue << current_node.right
 
       if block_given?
         yield(queue[0])
@@ -180,29 +182,62 @@ class Tree
     end
   end
 
+  def inorder
+
+  end
+
+  def preorder
+
+  end
+
+  def postorder
+
+  end
+
+  def height(value)
+
+  end
+
+  def depth(value)
+    current_node = root
+    depth_count = 0
+
+    while current_node != nil
+      return p depth_count if current_node.value == value
+      if current_node.value < value
+        previous_node = current_node
+        current_node = current_node.right
+        depth_count += 1
+
+      else
+        previous_node = current_node
+        current_node = current_node.left
+        depth_count += 1
+      end
+    end
+  end
+
   def pretty_print(node = @root, prefix = '', is_left = true)
-    pretty_print(node.right_node, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right_node
+    pretty_print(node.right, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right
     puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.value}"
-    pretty_print(node.left_node, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left_node
+    pretty_print(node.left, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left
   end
 end
 
 
 class Node
-  attr_accessor :value, :left_node, :right_node
-  def initialize(value, left_node = nil, right_node = nil)
+  attr_accessor :value, :left, :right
+  def initialize(value, left = nil, right = nil)
     @value = value
-    @left_node = left_node
-    @right_node = right_node
+    @left = left
+    @right = right
   end
 end
 
 ###########################################################################################
 BST = Tree.new([1, 7, 4, 23, 8, 9, 4, 3, 40, 5, 7, 9, 67, 6345, 324])
 
-BST.level_order
-BST.level_order{|element| p element.value}
-
+BST.delete(9)
 puts "---------------------------------------------"
 BST.pretty_print
 
