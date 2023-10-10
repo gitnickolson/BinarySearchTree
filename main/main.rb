0bad5b73229560d6f7efc7839cd6554737fbcf8f
gitnickolson <nicknickolson@web.de>
2023-10-10 21:34:33 +0200

    Additions - Added postorder method

--- main/main.rb
+++ main/main.rb
@@ -336,7 +336,7 @@ BST = Tree.new([1, 7, 4, 23, 8, 9, 4, 3, 40, 5, 7, 9, 67, 6345, 324])
 
 puts 'Level order:'
 p BST.level_order
-# BST.level_order{|element| p element}
+ BST.level_order{|element| p element}
 
 puts ''
 
